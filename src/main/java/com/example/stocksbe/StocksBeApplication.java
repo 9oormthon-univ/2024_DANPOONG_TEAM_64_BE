33cc6b02f0fc15a5729ea39c584cc8fcea3c1cbc
hyunji <phj9573@naver.com>
2024-11-19 00:11:24 +0900

    first commit

---
+++ src/main/java/com/example/stocksbe/StocksBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.stocksbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksBeApplication.class, args);
+    }
+
+}
